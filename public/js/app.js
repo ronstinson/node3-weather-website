5ff37721fca8dcd54c5f973653cd4af2da6d6225
ronstinson <37646400+ronstinson@users.noreply.github.com>
2020-07-03 20:47:50 -0500

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -7,17 +7,20 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
-
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+
+
+
+    fetch('http://localhost:3000/weather?address='+ location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
             } else {
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forecast
+                
             }
         })
     })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,9 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'http://api.weatherstack.com/current?access_key=64466aba507daae0243daa1a561aacc4&query=' + latitude + ',' + longitude
+
+    
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,9 +11,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions + ', It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
         }
     })
 }
+
 
 module.exports = forecast
