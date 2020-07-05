a8b1ad6db9f1c743f6f2d5f33d5faf3cacfe5869
ronstinson <37646400+ronstinson@users.noreply.github.com>
2020-07-05 16:32:37 -0500

    Removed CORS

--- src/app.js
+++ src/app.js
@@ -7,11 +7,11 @@ const forecast = require('./utils/forecast')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use((req, res, next) =>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use((req, res, next) =>{
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 
 
