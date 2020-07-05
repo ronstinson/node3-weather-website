4593ecca87a74ddb4f9760a27940026c1a034f11
ronstinson <37646400+ronstinson@users.noreply.github.com>
2020-07-05 17:53:46 -0500

    global nodemon

--- src/app.js
+++ src/app.js
@@ -7,13 +7,6 @@ const forecast = require('./utils/forecast')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) =>{
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
-
-
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
