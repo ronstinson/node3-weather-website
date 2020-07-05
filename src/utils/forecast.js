2d3a3021ade9a6fcba655087fa36627b26ec2ee3
ronstinson <37646400+ronstinson@users.noreply.github.com>
2020-07-05 12:38:32 -0500

    Added wind speed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions + ', It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions + ', It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.'+ 'Wind Speed is ' +body.current.wind_speed + ' knots.')
         }
     })
 }
